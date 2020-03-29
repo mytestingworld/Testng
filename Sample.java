59faf554a56db398b57b9ace5d5e0e4bcf0fb6be
mytestingworld <62851156+mytestingworld@users.noreply.github.com>
2020-03-29 14:17:34 -0400

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+/**
+ * Basic Example using Testng
+ */
+public class Sample {
+
+    @Test
+    public void testA()
+    {
+        System.out.println("Test");
+    }
+}
